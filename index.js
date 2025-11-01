559f5264eb297446680f5352662e860ac3bf503d
saket-bhadada <saketbhadada@gmail.com>
2025-11-01 22:11:38 +0530

    intializing

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { fetch } from "fetch";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+app.listen()
