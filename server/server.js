b69b1f81535ee572b549f0663d1c10b8d9e6faf9
saket-bhadada <saketbhadada@gmail.com>
2025-11-07 18:08:20 +0530

    root package.json

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.get("/",(req,res)=>{
-    res.json({"user":["userone","usertwo","userthree"]});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+app.get("/*",(req,res)=>{
+    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
 });
 
 app.listen(port,()=>{
