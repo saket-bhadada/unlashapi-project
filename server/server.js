9f5864ac2cc6a0e2bf77179700f9b538d2a3aa89
saket-bhadada <saketbhadada@gmail.com>
2025-12-17 23:17:24 +0530

    starting

--- server/server.js
+++ server/server.js
@@ -1,28 +1,15 @@
 import express from 'express';
-import path from 'path';
 import bodyParser from 'body-parser';
-import { fileURLToPath } from 'url';
 import cors from 'cors';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
 const app = express();
-const port = 5000;
+const port = 3000;
 
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials: true,
+}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(
-{    origin:["http://localhost:3000"]
-}))
-// Serve static files (JS, CSS, etc.)
-// app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
+app.use(express.json());
 
-// Fallback for SPA routes
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from the server!' });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// app.get('/home',homePage);
