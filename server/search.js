649e5fe77892b846407d6fed550d448ba8a17844
saket-bhadada <saketbhadada@gmail.com>
2026-01-26 22:47:40 +0530

    adding backend call for api

--- server/search.js
+++ server/search.js
@@ -2,9 +2,28 @@ import express from "express";
 
 const search = express.Router();
 
-search.post("/",(req,res)=>{
+search.post("/",async(req,res)=>{
     const { searchQuery } = req.body;
-    console.log("Search query:", searchQuery);
+    if(!searchQuery){
+        return res.status(400).json({error:"Search query is required"});
+    }else{
+        try{
+            api = process.env.API_KEY;
+            url = `https://api.unsplash.com/photos/?client_id=${api}&query=${searchQuery}S`;
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            if(response.ok){
+                return res.status(200).json({data});
+            }else{
+                return res.status(400).json({error:"Something went wrong"});
+            }
+        }catch(error){
+        console.error(error);
+        return res.status(500).json({error:"Something went wrong"});
+    }
+    }
+    // console.log("Search query:", searchQuery);
     res.json({ message: "Search successful", images: [] });
 });
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import homePage from './home.js';
