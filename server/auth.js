7ade14a483cafaf888112f3a7c1a739fa930f2e3
saket-bhadada <saketbhadada@gmail.com>
2026-01-31 23:06:24 +0530

    continuing with auth

--- server/auth.js
+++ server/auth.js
@@ -3,6 +3,23 @@ import express from "express";
 
 const auth = express.Router();
 
-auth.get('/login',passport.authenticate('google',{
-    scope:['profile','email']
-}))
+auth.get('/login',
+    passport.authenticate("local",{
+        successRedirect:"/",
+        failureRedirect:"/registration"
+    })
+)
+
+auth.post('/registration',async(req,res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmpassword = req.body.confirmpassword;
+
+    if(password !== confirmpassword){
+        return res.status(400).json({error:"Password does not match"});
+    }
+    try{}catch(error){        
+        res.redirect('/registration');
+        return(error);
+    }
+});
