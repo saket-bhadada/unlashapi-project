fed7d2d125309bc97df5e61b1e3d50c88d594560
saket-bhadada <saketbhadada@gmail.com>
2026-02-02 19:52:25 +0530

    initializing querys

--- server/auth.js
+++ server/auth.js
@@ -10,6 +10,7 @@ const db = new Client({
     port: process.env.DB_PORT,
 })
 const auth = express.Router();
+const saltround = 10;
 
 auth.get('/login',
     passport.authenticate("local",{
@@ -19,15 +20,44 @@ auth.get('/login',
 )
 
 auth.post('/registration',async(req,res)=>{
-    const email = req.body.email;
+    const userid = req.body.email;
     const password = req.body.password;
     const confirmpassword = req.body.confirmpassword;
 
     if(password !== confirmpassword){
         return res.status(400).json({error:"Password does not match"});
     }
-    try{}catch(error){        
+    try{
+        const checkResult = await db.query('SELECT * FROM users WHERE userid = $1',[userid]);
+        if(checkResult.rows.length > 0){
+            req.redirect('/registration');
+        }else{
+            bcrypt.hash(password,saltround,async(err, hash)=>{
+                if(err){
+                    console.error(err);
+                }else{
+                    const result = await db.query('INSERT INTO users (userid,password) VALUES ($1,$2)',[userid,hash]);
+                    const user = result.rows[0];
+                    req.login(user,(err)=>{
+                        res.redirect('/home');
+                    })
+                }
+            });
+        }
+    }catch(error){        
         res.redirect('/registration');
         return(error);
     }
 });
+
+passport.use(
+    "google",
+    new googleStrategy(
+        {}
+    )
+);
+
+passport.use(
+    "local",
+    new Strategy(async function )
+)
