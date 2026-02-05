178887ea924805c03f864880571f125250a1d71c
saket-bhadada <saketbhadada@gmail.com>
2026-02-05 22:40:53 +0530

    chat react updating

--- client/src/chat.jsx
+++ client/src/chat.jsx
@@ -2,3 +2,18 @@ import React,{useState,useEffect} from "react";
 import io from 'socket.io-client';
 
 const socket = io.connect("httpt://localhost:3000");
+
+function App(){
+    const [message,setmessage] = useState("");
+    const [chat,setchat] = useState([]);
+    useEffect(()=>{
+        socket.on('message',(data)=>{
+            setchat((prev)=>[...prev,data]);
+        })
+    },[]);
+
+    const sendmessage = (username)=>{
+        socket.emit('message',{username:{username},content:message});
+        setmessage("");
+    }
+}
